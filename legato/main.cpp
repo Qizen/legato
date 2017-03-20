2d7683956fdcf55cd0debe7084a9356af26aef89
Adam K. Schmidt <aschmidt@tcd.ie>
2017-03-20 00:03:21 +0000

    lighthouse init

--- legato/main.cpp
+++ legato/main.cpp
@@ -4,10 +4,11 @@
 #include <opencv2/imgproc.hpp>
 #include <iostream>
 
-#define IMAGE_NAME "BSDS_62.jpg"
-//#define IMAGE_NAME "image3.jpg"
+//#define IMAGE_NAME "BSDS_62.jpg"
+#define IMAGE_NAME "image4.jpg"
 #define PI 3.14159265
 #define DEBUG 0
+#define SCORE_THRESH 0.7
 
 using namespace cv;
 using namespace std;
@@ -29,6 +30,14 @@ void trackCallback(int trackerValue, void* userData)
 	GaussianBlur(*img, blurred, Size(5, 5), 0);
 	Canny(blurred, blurCanny, trackerValue, trackerValue*ratio);
 	imshow("Canny Blurred", blurCanny);
+}
+
+void threshCallback(int trackerValue, void* userData)
+{
+	Mat* img = static_cast<Mat*>(userData);
+	Mat thresh;
+	threshold(*img, thresh, trackerValue, 255, THRESH_BINARY);
+	imshow("thresh", thresh);
 }
 
 void sobel(Mat& inputImage, Mat& outputX, Mat& outputY)
@@ -137,6 +146,28 @@ float f3[3][3] = { { 1.5, 1.3, 1.0 },
 { 1.3, 1.0, 1.3 },
 { 1.0, 1.3, 1.5 } };
 Mat w3(Size(3, 3), CV_32F, &f3);
+
+uchar u90[9][9] = { {0,0,0,1,1,1,0,0,0},
+					{0,0,0,1,1,1,0,0,0},
+					{0,0,0,1,1,1,0,0,0},
+					{0,0,0,1,1,1,0,0,0},
+					{0,0,0,1,0,1,0,0,0},
+					{0,0,0,0,0,0,0,0,0},
+					{0,0,0,0,0,0,0,0,0},
+					{0,0,0,0,0,0,0,0,0},
+					{0,0,0,0,0,0,0,0,0} };
+Mat fil90(Size(9, 9), CV_8U, &u90);
+
+uchar u60[9][9] = { { 0,0,0,0,0,1,1,1,0 },
+					{ 0,0,0,0,1,1,1,1,0 },
+					{ 0,0,0,0,1,1,1,0,0 },
+					{ 0,0,0,0,1,1,1,0,0 },
+					{ 0,0,0,0,0,1,0,0,0 },
+					{ 0,0,0,0,0,0,0,0,0 },
+					{ 0,0,0,0,0,0,0,0,0 },
+					{ 0,0,0,0,0,0,0,0,0 },
+					{ 0,0,0,0,0,0,0,0,0 } };
+Mat fil60(Size(9, 9), CV_8U, &u60);
 
 int pixInEdgeCount = 0;
 
@@ -381,6 +412,102 @@ void pathfind(Mat& inputEdges, Mat& inputOrientations, Mat& output)
 	output = out;
 }
 
+void neighbourVote(Mat& inputEdges, Mat& inputOrientations, int radius, Mat& output, int dirPow, int colPow, double thresh )
+{
+	//TODO: borders
+	int val;
+	double ornt;
+	int numVotes = pow((radius * 2 + 1), 2) - 1;
+
+	output = output.zeros(inputEdges.size(), CV_32F);
+
+	Mat orientRad;
+	orientRad = orientRad.zeros(inputOrientations.size(), CV_32F);
+	for (int y = 0; y < orientRad.rows; y++)
+	{
+		for (int x = 0; x < orientRad.cols; x++)
+		{
+			orientRad.at<float>(y, x) = (float)inputOrientations.at<uchar>(y, x) * PI / 90.0;
+		}
+	}
+
+	//orientRad = inputOrientations * PI / 90.0;
+
+	Mat pixAngles;
+	pixAngles = pixAngles.zeros(Size(radius * 2 + 1, radius * 2 + 1), CV_32F);
+
+	for (int y = 0; y <= radius * 2; y++)
+	{
+		for (int x = 0; x <= radius * 2; x++)
+		{
+			// y coords swapped to change from y-down (image) to y-up (coord space)
+			pixAngles.at<float>(y, x) = atan2(radius - y, x - radius);
+		}
+	}
+
+	for (int y = radius; y < (inputEdges.rows - radius); y++)
+	{
+		for (int x = radius; x < (inputEdges.cols - radius); x++)
+		{
+			ornt = inputOrientations.at<uchar>(y, x) * PI / 90.0;
+			val = inputEdges.at<uchar>(y, x);
+			float score = 0;
+
+#if DEBUG
+			Mat scores;
+			scores = scores.zeros(Size(radius * 2 + 1, radius * 2 + 1), CV_32F);
+
+			Mat dirmat;
+		    dirmat = dirmat.zeros(Size(radius * 2 + 1, radius * 2 + 1), CV_32F);
+			Mat colmat;
+			colmat =colmat.zeros(Size(radius * 2 + 1, radius * 2 + 1), CV_32F);
+#endif
+			//in local region
+			for (int j = (y - radius); j <= (y + radius); j++)
+			{
+				for (int i = (x - radius); i <= (x + radius); i++)
+				{
+					//pass over central pixel
+					if (j == y && i == x) continue;
+					
+					//double or_n = inputOrientations.at<uchar>(j, i) * PI / 90.0;
+					float or_n = orientRad.at<float>(j, i);
+					float v_n = inputEdges.at<uchar>(j, i);
+					//double dir = pow(cos(/*ornt -*/ or_n), 3);
+
+					// atan2 passed a 0 value will crash?
+					// we should precompute at least the atan
+					// y coords swapped to change from y-down (image) to y-up (coord space)
+					double phi = pixAngles.at<float>(j - (y - radius), i - (x - radius)); //= atan2(y - j, i - x);
+					double col = abs(cos(ornt + PI/2.0 - phi));
+
+					double diff = abs(ornt - or_n);
+					if (diff > PI)
+						diff = 2 * PI - diff;
+
+					if (diff > 0.3)
+						continue;
+#if DEBUG
+					scores.at<float>(j - (y - radius), i - (x - radius)) = diff<0.3 ? col*v_n/255 : 0;
+					dirmat.at<float>(j - (y - radius), i - (x - radius)) = abs(ornt - or_n)<0.3?1:0;
+					colmat.at<float>(j - (y - radius), i - (x - radius)) = col;
+#endif				
+					double posScore = pow(col, colPow) * v_n / 255;
+
+					if (posScore > thresh)
+						score += posScore;
+				}
+			}
+
+			float weight = 8 * score / numVotes;
+			//note: output does not currently depend on value of middle pixel
+			// -- probably causing some fuzzing but does allow for stronger filled-in lines
+			output.at<float>(y, x) = 255 * weight;
+			//local region
+		}
+	}
+}
+
 void printMat(Mat& m)
 {
 	for (int y = 0; y < m.rows; y++)
@@ -393,7 +520,338 @@ void printMat(Mat& m)
 	}
 }
 
-int main(int argc, char** argv)
+int g_dirPow = 1;
+int g_colPow = 1;
+double g_thresh = 0.7;
+
+struct SobImg  {
+	Mat* edges;
+	Mat* orientations;
+};
+
+void dirCallback(int trackerValue, void* userData)
+{
+	SobImg* simg = static_cast<SobImg*>(userData);
+	g_dirPow = trackerValue;
+	Mat out;
+	neighbourVote(*(simg->edges), *(simg->orientations), 3, out, g_dirPow, g_colPow, g_thresh);
+	
+	double max;
+	minMaxLoc(out, nullptr, &max);
+
+	out = out * 255 / max;
+	Mat voting255;
+	out.convertTo(voting255, CV_8U);
+
+	imshow("voting", voting255);
+}
+
+void colCallback(int trackerValue, void* userData)
+{
+	SobImg* simg = static_cast<SobImg*>(userData);
+	g_colPow = trackerValue;
+	Mat out;
+	neighbourVote(*(simg->edges), *(simg->orientations), 3, out, g_dirPow, g_colPow, g_thresh);
+
+	double max;
+	minMaxLoc(out, nullptr, &max);
+
+	out = out * 255 / max;
+	Mat voting255;
+	out.convertTo(voting255, CV_8U);
+
+	imshow("voting", voting255);
+}
+void thrCallback(int trackerValue, void* userData)
+{
+	SobImg* simg = static_cast<SobImg*>(userData);
+	g_thresh = trackerValue/100.0;
+	Mat out;
+	neighbourVote(*(simg->edges), *(simg->orientations), 3, out, g_dirPow, g_colPow, g_thresh);
+
+	double max;
+	minMaxLoc(out, nullptr, &max);
+
+	out = out * 255 / max;
+	Mat voting255;
+	out.convertTo(voting255, CV_8U);
+
+	imshow("voting", voting255);
+}
+
+void lighthouse(Mat& inputEdges, Mat& inputOrientations, Mat&output)
+{
+	output = output.zeros(inputEdges.size(), inputEdges.type());
+	int radius = 4;
+	
+	Mat output0;
+	output0 = output0.zeros(inputEdges.size(), inputEdges.type());
+
+	Mat output30;
+	output30 = output0.clone();
+
+	Mat output60;
+	output60 = output60.zeros(inputEdges.size(), inputEdges.type());
+
+	Mat output90;
+	output90 = output0.clone();
+
+	Mat output120;
+	output120 = output0.clone();
+
+	Mat output150;
+	output150 = output0.clone();
+
+	Mat output180;
+	output180 = output0.clone();
+
+	Mat output210;
+	output210 = output0.clone();
+
+	Mat output240;
+	output240 = output0.clone();
+
+	Mat output270;
+	output270 = output270.zeros(inputEdges.size(), inputEdges.type());
+
+	Mat output300;
+	output300 = output0.clone();
+
+	Mat output330;
+	output330 = output0.clone();
+	
+	for (int y = radius+1; y < (inputEdges.rows - radius); y++)
+	{
+		for (int x = radius+1; x < (inputEdges.cols - radius); x++)
+		{
+			Mat localEdges = (inputEdges)(Rect(x - radius, y - radius, 9, 9)).clone();
+			Mat localOrientations = (inputOrientations)(Rect(x - radius, y - radius, 9, 9)).clone();
+			
+			Mat res0;
+			res0.create(localEdges.size(), localEdges.type());
+			Mat fil0;
+			transpose(fil90, fil0);
+			flip(fil0, fil0, 1);
+			res0 = localEdges.mul(fil0);
+
+			Mat res30;
+			res30.create(localEdges.size(), localEdges.type());
+			Mat fil30;
+			transpose(fil60, fil30);
+			flip(fil30, fil30, -1);
+			res30 = localEdges.mul(fil30);
+			
+			Mat res60;
+			res60.create(localEdges.size(), localEdges.type());
+			res60 = localEdges.mul(fil60);
+			
+			//for 90 degrees
+			Mat res90;
+			res90.create(localEdges.size(), localEdges.type());
+			res90 = localEdges.mul(fil90);
+			
+			Mat res120;
+			res120.create(localEdges.size(), localEdges.type());
+			Mat fil120;
+			flip(fil60, fil120, 1);
+			res120 = localEdges.mul(fil120);
+
+			Mat res150;
+			res150.create(localEdges.size(), localEdges.type());
+			Mat fil150;
+			transpose(fil60, fil150);
+			flip(fil150, fil150, 0);
+			res150 = localEdges.mul(fil150);
+
+			Mat res180;
+			res180.create(localEdges.size(), localEdges.type());
+			Mat fil180;
+			transpose(fil90, fil180);
+			res180 = localEdges.mul(fil180);
+
+			Mat res210;
+			res210.create(localEdges.size(), localEdges.type());
+			Mat fil210;
+			transpose(fil60, fil210);
+			//flip(fil210, fil210, 0);
+			res210 = localEdges.mul(fil210);
+
+			Mat res240;
+			res240.create(localEdges.size(), localEdges.type());
+			Mat fil240;
+			flip(fil60, fil240, -1);
+			res240 = localEdges.mul(fil240);
+
+			Mat res270;
+			res270.create(localEdges.size(), localEdges.type());
+			Mat fil270;
+			flip(fil90, fil270, 0);
+			res270 = localEdges.mul(fil270);
+
+			Mat res300;
+			res300.create(localEdges.size(), localEdges.type());
+			Mat fil300;
+			flip(fil60, fil300, 0);
+			res300 = localEdges.mul(fil300);
+
+			Mat res330;
+			res330.create(localEdges.size(), localEdges.type());
+			Mat fil330;
+			transpose(fil60, fil330);
+			flip(fil330, fil330, 1);
+			res330 = localEdges.mul(fil330);
+		
+			uchar central_ornt = inputOrientations.at<uchar>(y, x);
+			uchar central_mag = inputEdges.at<uchar>(y, x);
+
+			int score0 = 0;
+			int score30 = 0;
+			int score60 = 0;
+			int score90 = 0;
+			int score120 = 0;
+			int score150 = 0;
+			int score180 = 0;
+			int score210 = 0;
+			int score240 = 0;
+			int score270 = 0;
+			int score300 = 0;
+			int score330 = 0;
+
+			int angleThresh = 5;
+			for (int j = 0; j <= 2 * radius; j++)
+			{
+				for (int i = 0; i <= 2 * radius; i++)
+				{
+					if (i == radius && j == radius) continue; // central pixel
+					int ornt = (int)localOrientations.at<uchar>(j, i);
+					
+					//0 and 180
+					if (abs(ornt - 45) < angleThresh || abs(ornt - 135) < angleThresh)
+					{
+						score0 += res0.at<uchar>(j, i);
+						score180 += res180.at<uchar>(j, i);
+					}
+					
+					//30 and 210
+					if (abs(ornt - 60) < angleThresh || abs(ornt - 150) < angleThresh)
+					{
+						score30 += res30.at<uchar>(j, i);
+						score210 += res210.at<uchar>(j, i);
+					}
+					
+					//60 and 240
+					if (abs(ornt - 75) < angleThresh || abs(ornt - 165) < angleThresh)
+					{
+						score60 += res60.at<uchar>(j, i);
+						score240 += res240.at<uchar>(j, i);
+					}
+
+					//90 and 270
+					if (abs(ornt - 90) < angleThresh || abs(ornt - 180) < angleThresh || ornt < angleThresh)
+					{
+						score90 += res90.at<uchar>(j, i);
+						score270 += res270.at<uchar>(j, i);
+					}
+					
+					//120 and 300
+					if (abs(ornt - 105) < angleThresh || abs(ornt - 195) < angleThresh)
+					{
+						score120 += res120.at<uchar>(j, i);
+						score300 += res300.at<uchar>(j, i);
+					}
+
+					//150 and 330
+					if (abs(ornt - 120) < angleThresh || abs(ornt - 210) < angleThresh)
+					{
+						score150 += res150.at<uchar>(j, i);
+						score330 += res330.at<uchar>(j, i);
+					}
+					
+				}
+			}
+			score0 /= 10;
+			score30 /= 10;
+			score60 /= 10;
+			score90 /= 10;
+			score120 /= 10;
+			score150 /= 10;
+			score180 /= 10;
+			score210 /= 10;
+			score240 /= 10;
+			score270 /= 10;
+			score300 /= 10;
+			score330 /= 10;
+			
+			output0.at<uchar>(y, x) = score0 * central_mag / 255;
+			output30.at<uchar>(y, x) = score30 * central_mag / 255;
+			output60.at<uchar>(y, x) = score60 * central_mag / 255;
+			output.at<uchar>(y, x) = score90 * central_mag / 255;
+			output120.at<uchar>(y, x) = score120 * central_mag / 255;
+			output150.at<uchar>(y, x) = score150 * central_mag / 255;
+			output180.at<uchar>(y, x) = score180 * central_mag / 255;
+			output210.at<uchar>(y, x) = score210 * central_mag / 255;
+			output240.at<uchar>(y, x) = score240 * central_mag / 255;
+			output270.at<uchar>(y, x) = score270 * central_mag / 255;
+			output300.at<uchar>(y, x) = score300 * central_mag / 255;
+			output330.at<uchar>(y, x) = score330 * central_mag / 255;
+		}
+	}
+}
+
+void quickTest()
+{
+	Mat image;
+	image = imread(IMAGE_NAME, IMREAD_GRAYSCALE);
+	Mat resized_image;// = image;
+	resize(image, resized_image, Size(), 0.2, 0.2, 1);
+	Mat outputX, outputY;
+	sobel(resized_image, outputX, outputY);
+	Mat s;
+	sobelCombineXY(outputX, outputY, s);
+	imshow("sobel", s);
+
+	Mat orientations;
+	sobelOrientations(outputX, outputY, orientations);
+
+	int radius = 5;
+	float thr = 0.35; //0.35
+	Mat voting;
+	neighbourVote(s, orientations, radius, voting, 9, 1, thr);
+
+	double max;
+	minMaxLoc(voting, nullptr, &max);
+
+	voting = voting * 255 / max;
+	Mat voting255;
+	voting.convertTo(voting255, CV_8U);
+	imshow("legato", voting255);
+
+	Mat itVoting;
+	neighbourVote(voting255, orientations, radius, itVoting, 9, 1, thr);
+	Mat itVot255;
+	minMaxLoc(itVoting, nullptr, &max);
+
+	itVoting = itVoting * 255 / max;
+	itVoting.convertTo(itVot255, CV_8U);
+	imshow("iterated legato", itVot255);
+	Mat thIt;
+	threshold(itVot255, thIt, 20, 255, THRESH_BINARY);
+	imshow("threshed", thIt);
+
+	Mat it2Voting;
+	neighbourVote(itVot255, orientations, radius, it2Voting, 9, 1, thr);
+	Mat it2Vot255;
+	minMaxLoc(it2Voting, nullptr, &max);
+
+	it2Voting = it2Voting * 255 / max;
+	it2Voting.convertTo(it2Vot255, CV_8U);
+	imshow("triple legato", it2Vot255);
+
+	waitKey(0);
+	return;
+}
+
+int testing()
 {
 	//just testing array assignment
 	float arr[3][3] = { {1, 1, 1}
@@ -411,8 +869,8 @@ int main(int argc, char** argv)
 		return -1;
 	}
 
-	Mat resized_image = image;
-	//resize(image, resized_image, Size(), 0.2, 0.2, 1);
+	Mat resized_image;// = image;
+	resize(image, resized_image, Size(), 0.2, 0.2, 1);
 	namedWindow("Display window", WINDOW_NORMAL); // Create a window for display.
 	imshow("Display window", resized_image); // Show our image inside it.
 
@@ -452,18 +910,98 @@ int main(int argc, char** argv)
 
 	imshow("orientations as colours", bgr_orientations);
 	Mat ogSobel;
-	Sobel(resized_image, ogSobel, -1, 1, 0);
+	Sobel(resized_image, ogSobel, -1, 1, 1);
 	imshow("Their Sobel X", ogSobel);
 
 
 	namedWindow("out", WINDOW_NORMAL);
-	waitKey(0);
+	//waitKey(0);
 
-	Mat legato;
+	/*Mat legato;
 	pathfind(s, orientations, legato);
 	namedWindow("legato", WINDOW_NORMAL);
 	imshow("legato", legato);
+	*/
+
+	//blurring just makes things worse
+	//Mat blurS;
+	//GaussianBlur(s, blurS, Size(3, 3), 0);
+
+	Mat voting;
+	neighbourVote(s, orientations, 5, voting, 9, 1, 0.35);	
+
+	double max;
+	minMaxLoc(voting, nullptr, &max);
+
+	voting = voting * 255 / max;
+	Mat voting255;
+	voting.convertTo(voting255, CV_8U);
+	
+	Mat itVoting;
+	neighbourVote(voting255, orientations, 5, itVoting, 9, 1, 0.35);
+	Mat itVot255;
+	minMaxLoc(itVoting, nullptr, &max);
+
+	itVoting = itVoting * 255 / max;
+	itVoting.convertTo(itVot255, CV_8U);
+	imshow("iterated legato", itVot255);
+	Mat thIt;
+	threshold(itVot255, thIt, 20, 255, THRESH_BINARY);
+	imshow("threshed", thIt);
+
+	Mat it2Voting;
+	neighbourVote(itVot255, orientations, 5, it2Voting, 9, 1, 0.35);
+	Mat it2Vot255;
+	minMaxLoc(it2Voting, nullptr, &max);
+
+	it2Voting = it2Voting * 255 / max;
+	it2Voting.convertTo(it2Vot255, CV_8U);
+	imshow("triple legato", it2Vot255);
+
+	namedWindow("voting", WINDOW_NORMAL); // Create a window for display.
+	imshow("voting", voting255);
+	imshow("single legato", voting255);
+	namedWindow("thresh", WINDOW_NORMAL);
+	createTrackbar("Threshold", "thresh", nullptr, 255, threshCallback, &voting255);
+	threshCallback(128, &voting255);
+
+	SobImg simg;
+	simg.edges = &s;
+	simg.orientations = &orientations;
+
+	createTrackbar("Dir", "voting", nullptr, 10, dirCallback, &simg);
+	createTrackbar("Col", "voting", nullptr, 10, colCallback, &simg);
+	createTrackbar("Thresh", "voting", nullptr, 100, thrCallback, &simg);
+
 
 	waitKey(0);
+	return 0;
+}
+
+void lighthouseTest()
+{
+	Mat image;
+	image = imread(IMAGE_NAME, IMREAD_GRAYSCALE);
+	Mat resized_image = image;
+	//resize(image, resized_image, Size(), 0.2, 0.2, 1);
+	Mat outputX, outputY;
+	sobel(resized_image, outputX, outputY);
+	Mat s;
+	sobelCombineXY(outputX, outputY, s);
+	imshow("sobel", s);
+
+	Mat orientations;
+	sobelOrientations(outputX, outputY, orientations);
+
+	Mat output;
+
+	lighthouse(s, orientations, output);
+	return;
+}
+
+int main(int argc, char** argv)
+{
+	lighthouseTest();
+	//quickTest();
 	return 0;
 }
